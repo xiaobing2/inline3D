1f209648d78b7708be2c4467a9c420a6244e113b
come_2028 <82049698@qq.com>
2025-12-14 15:42:53 +0800

    更新

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    output: {
+      filename: 'js/[name].js',
+      chunkFilename: 'js/[name].js'
+    }
+  },
+  css: {
+    extract: {
+      filename: 'css/[name].css',
+      chunkFilename: 'css/[name].css'
+    }
+  }
 })
